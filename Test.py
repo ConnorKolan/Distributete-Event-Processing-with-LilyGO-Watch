559b35938685f1d75f2187ed96a037d4423c02d3
ConnorKolan <71281093+ConnorKolan@users.noreply.github.com>
2023-07-05 20:40:40 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,33 +1,42 @@
 import requests
 
-url = "http://192.168.178.59/post"  # Replace <IP_Address> with the actual IP address of your server
+url = "http://192.168.178.59/ip"  # Replace <IP_Address> with the actual IP address of your server
 
-payload = {
-    'type': "Siddhi",                       #siddhi or metadata
-    'message':  "define stream StockStream (symbol string, price int, volume int); " +
+"""
+"define stream StockStream (symbol string, price int, volume int); " +
                 "@info(name = 'query1') " +
                 "from StockStream[volume < 150] " +
                 "select symbol, price " +
-                "insert into OutputStream;",
-    'events': "Gyro, Touchscreen",          #Touchscreen,  joystick, Accel, Humidity
-    'values': "{}, {}",                     # has to be special for every sensor
-    'timeframe': "2",                       #in seconds
-    'operations': "&"
-}
-
-"""
-payload = {
-    'type': "time",                         #siddhi, event or time
-    'timestamp': ""
-}
+                "insert into OutputStream;"
 """
 
+"""payload = {                     #siddhi or metadata 
+    'events': "Gyro, Touchscreen, joystick",                        #Touchscreen,  joystick, Accel, Humidity
+    'values': "",                                                   # has to be special for every sensor
+    'timeframe': "2, 2, 2",                                         #in seconds
+    'operations': "&, &",
+    'origins': ""                                                   #device name
+    'destination': ""                                               #device name
+}"""
+
 """
-payload = {
-    'type': "event",                        #siddhi, event or time
-    'events': "Gyro, Touchscreen",          #Touchscreen,  joystick, Accel, Humidity
-    'values': "{}, {}",                     # has to be special for every sensor
-    'timestamp': "2"                        #in seconds
+payload = {                         #siddhi, event or time
+    'timestamp': "63800006400"
+}
+"""
+
+
+"""payload = {                         
+    'ip': "10.11.251.33",
+    'device': "pi1"           #either "watch" or "pi"
+}"""
+
+
+"""
+payload = {                      #siddhi, event or time
+    'event': "Gyro",                            #Touchscreen,  joystick, Accel, Humidity
+    'values': "True",                           # has to be special for every sensor
+    'timestamp': "42069"                        #in seconds
 }
 """
 
